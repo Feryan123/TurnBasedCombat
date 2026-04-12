ef3beb554e5245bbad1788183f58172641677963
minholly1993 <58905896+minholly1993@users.noreply.github.com>
2026-04-13 02:30:47 +0800

    Update Enemy.java

--- Combatants/Enemy.java
+++ Combatants/Enemy.java
@@ -1,24 +1,28 @@
 package Combatants;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import actions.Action;
+import actions.BasicAttack;
+import Control.BattleEngine;
+
 public abstract class Enemy extends Combatant{
-	private EnemyActionStrategy actionStrategy;
-	
 	public Enemy(String combatantName, int HP, int Atk, int Def, int Speed) {
 		super(combatantName, HP, Atk, Def, Speed);
 	}
 
+	@Override
 	public void takeTurn(BattleEngine engine) {
-		engine.spawnBackUpIfNeeded();
-		engine.applyTurnStartEffect();
-		if (!canAct()) return;
+		engine.processRound();
+		if (!canAct()) return; 
+		engine.processTurn(this);
 		Action action = decideAction(engine);
-		List<Combatants> targets = engine.getAliveCombatants();
+		List<Combatant> targets = engine.getAliveCombatants();
 		for (Combatant target : targets) {
-			action.execute(this, target, engine);
+			action.execute(this, target);
 		}
 		
 	}
-	public Action decideAction(BattleEngine engine) {
-		return new BasicAttack();
-	}
+	public Action decideAction(BattleEngine engine) { return new BasicAttack(); }
 }
