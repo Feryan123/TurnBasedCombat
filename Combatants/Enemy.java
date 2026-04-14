7767c89fc94e54f2559afb117c110f17535573fb
minholly1993 <58905896+minholly1993@users.noreply.github.com>
2026-04-14 18:38:49 +0800

    Update Enemy.java

--- Combatants/Enemy.java
+++ Combatants/Enemy.java
@@ -6,24 +6,22 @@ import Actions.Action;
 import Actions.BasicAttack;
 import Control.BattleEngine;
 
-public abstract class Enemy extends Combatant {
+public abstract class Enemy extends Combatant{
+	public Enemy(String combatantName, int HP, int Atk, int Def, int Speed) {
+		super(combatantName, HP, Atk, Def, Speed);
+	}
 
-    public Enemy(String combatantName, int HP, int Atk, int Def, int Speed) {
-        super(combatantName, HP, Atk, Def, Speed);
-    }
-
-    public void takeTurn(BattleEngine engine) {
-        if (!canAct()) return;
-
-        Action action = decideAction(engine);
-        Combatant target = engine.selectPlayerTarget();
-
-        if (target != null) {
-            action.execute(this, target);
-        }
-    }
-
-    public Action decideAction(BattleEngine engine) {
-        return new BasicAttack();
-    }
+	@Override
+	public void takeTurn(BattleEngine engine) {
+		engine.processRound();
+		if (!canAct()) return; 
+		engine.processTurn(this);
+		Action action = decideAction(engine);
+		List<Combatant> targets = engine.getAliveCombatants();
+		for (Combatant target : targets) {
+			action.execute(this, target);
+		}
+		
+	}
+	public Action decideAction(BattleEngine engine) { return new BasicAttack(); }
 }
